c82a18e5983d66026454d3c294dbdaf76aaf9967
rashedsalah <rashed.1991@gmail.com>
2020-06-02 18:44:12 +0300

    Add files via upload

---
+++ Tipsy-master/Tipsy-iOS13/Tipsy/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Tipsy
+//
+//  Created by Rashed Salah on 31.10.2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
